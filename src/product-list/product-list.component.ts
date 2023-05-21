074f30caa7c8e4226abc348729ebc650113db33f
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-05-21 11:14:04 -0500

    2 files modified, 4 added

---
+++ src/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  standalone:true
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
